c87ca888409b5445255e0ebe77b853f98379b8af
Harrf Akbar <harrf_akbar@yahoo.com>
2021-02-22 17:11:11 -0800

    commit tree 0a1af2466ac721798efa6f875e01be8803a6252e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+function writeCards(card) {
+    let arr = []; 
+    for (let i = 0; i < card.length; i++) {
+        arr.push(`Thank you, ${card[i]}, for the wonderful surprise gift!`)
+    }
+    return arr;
+}
+
